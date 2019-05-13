aa310d5525efb27ff85411dadf879d6c46d87dc6
Sarah Gibson <sgibson@turing.ac.uk>
2019-05-13 16:56:05 +0100

    Add blank echo command to upgrade.sh to add whitespace to output

--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,6 @@
 
 helm repo update
 helm upgrade $1 jupyterhub/binderhub --version=$2 -f .secret/secret.yaml -f .secret/config.yaml
+echo
 kubectl get pods -n $1
 echo "Binder IP: " `kubectl get svc binder -n $1 | awk '{ print $4}' | tail -n 1`
