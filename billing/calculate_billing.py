7886d1a4308319e186a3ef2f937451a37177238c
Sarah Gibson <sgibson@turing.ac.uk>
2019-06-10 19:39:35 +0100

    Refactor, add months argument

--- billing/calculate_billing.py
+++ billing/calculate_billing.py
@@ -1,95 +1,97 @@
 import yaml
 import argparse
-import datetime
 
 
 def parse_args():
     """
 Parse command line arguments.
 
-This function reads one command line argument which is the percentage contingency
-desired when calculating costs for autoscaling of the cluster. If not provided,
-this value defaults to 10%.
+This function reads one command line argument which is the percentage
+contingency desired when calculating costs for autoscaling of the cluster. If
+not provided, this value defaults to 10%.
     """
     parser = argparse.ArgumentParser(
-        description="Parse a contingency percentage for calculating BinderHub Cloud costs."
+        description=("Parse a contingency percentage for calculating BinderHub "
+                     "Cloud costs.")
     )
 
-    parser.add_argument("-c", "--contingency", default=10,
-                        help="Parse the percentage contingency you would like to account for "
-                             "autoscaling. Default value is 10%.")
+    parser.add_argument(
+        "-c", "--contingency", default=10, type=int,
+        help=("Parse the percentage contingency you would like to account " +
+              "for autoscaling. Default value is 10%.")
+    )
+    parser.add_argument(
+        "-m", "--months", default=1, type=int,
+        help=("Number of months to calculate Azure credits for. Default value "
+              + "is 1 month.")
+    )
 
     return parser.parse_args()
 
 
-def calculate_costs(billing_info, contingency):
+def calculate_costs(billing_info):
     """
-Function to calculate various monthly and yearly costs of the cluster and container
-registry required to run a BinderHub. A contingency value is parsed from the
-command line in order to account for autoscaling.
+Function to calculate various monthly and yearly costs of the cluster and
+container registry required to run a BinderHub. A contingency value is parsed
+from the command line in order to account for autoscaling.
 
     :param billing_info: a dict object containing the billing information
     :param contingency: the desired contingency percentage (float, 0 < contingency < 1)
     :return: updated instance of billing_info (dict)
     """
-    # Time stamp the calculations
-    billing_info["datetime_calculated_utc"] = f"{datetime.datetime.utcnow()}"
+    # Create empty dictionary
+    billing_info["costs"] = {}
 
     # Calculate total VM cost per month
-    billing_info["cluster"]["cost_permonth_usd"] = (
+    billing_info["costs"]["cluster_cost_permonth_usd"] = (
         billing_info["cluster"]["cost_pernode_permonth_usd"] *
         billing_info["cluster"]["node_number"]
     )
 
-    # Calculate total VM cost per year
-    billing_info["cluster"]["cost_peryear_usd"] = (
-        billing_info["cluster"]["cost_permonth_usd"] * 12
-    )
-
     # Calculate ACR cost per year
-    billing_info["acr"]["cost_peryear_usd"] = (
+    billing_info["costs"]["acr_cost_peryear_usd"] = (
         billing_info["acr"]["cost_perday_usd"] * 365.25
     )
 
     # Calculate average ACR cost per month
-    billing_info["acr"]["cost_peravgmonth_usd"] = (
-        billing_info["acr"]["cost_peryear_usd"] / 12
-    )
-
-    # Calculate monthly costs
-    billing_info["costs_usd"] = {"monthly":
-        billing_info["cluster"]["cost_permonth_usd"] +
-        billing_info["acr"]["cost_peravgmonth_usd"]
-    }
-
-    # Calculate yearly costs
-    billing_info["costs_usd"]["yearly"] = (
-        billing_info["cluster"]["cost_peryear_usd"] +
-        billing_info["acr"]["cost_peryear_usd"]
-    )
-
-    # Calculate monthly costs with contingency
-    billing_info["costs_usd"]["monthly_contingency"] = (
-        billing_info["costs_usd"]["monthly"] * (1.0 + contingency)
-    )
-
-    # Calculate yearly costs with contingency
-    billing_info["costs_usd"]["yearly_contingency"] = (
-        billing_info["costs_usd"]["yearly"] * (1.0 + contingency)
+    billing_info["costs"]["acr_cost_peravgmonth_usd"] = (
+        billing_info["costs"]["acr_cost_peryear_usd"] / 12
     )
 
     return billing_info
 
 
-def summary_stats(yml, contingency):
+def summary_stats(yml, contingency, months):
     # Print cost breakdown
-    print("""
-Total yearly cost (no contingency): ${0:.2f}
-Total yearly cost with {1}% contingency: ${2:.2f} ** PASTE THIS VALUE INTO TOPDESK **
-""".format(
-        yml["costs_usd"]["yearly"], contingency,
-        yml["costs_usd"]["yearly_contingency"]
-    ))
+    if months != 1:
+        print(f"""
+Cluster cost per month: ${yml["costs"]["cluster_cost_permonth_usd"]:.2f}
+ACR cost per month: ${yml["costs"]["acr_cost_peravgmonth_usd"]:.2f}
+
+For {months} months:
+Cluster cost per month: ${months * yml["costs"]["cluster_cost_permonth_usd"]:.2f}
+ACR cost per month: ${months * yml["costs"]["acr_cost_peravgmonth_usd"]:.2f}
+
+For {months} months with {contingency}% contingency:
+Cluster cost per month: ${(1.0 + contingency) * months * yml["costs"]["cluster_cost_permonth_usd"]:.2f}
+ACR cost per month: ${(1.0 + contingency) * months * yml["costs"]["acr_cost_peravgmonth_usd"]:.2f}
+
+Total cost for {months} months with {contingency}% contingency:
+${(1.0 + contingency) * months * (yml["costs"]["cluster_cost_permonth_usd"] + yml["costs"]["acr_cost_peravgmonth_usd"]):.2f}
+""")
+
+    else:
+        print(f"""
+Cluster cost per month: ${yml["costs"]["cluster_cost_permonth_usd"]:.2f}
+ACR cost per month: ${yml["costs"]["acr_cost_peravgmonth_usd"]:.2f}
+
+With {contingency}% contingency:
+Cluster cost per month: ${(1.0 + contingency) * yml["costs"]["cluster_cost_permonth_usd"]:.2f}
+ACR cost per month: ${(1.0 + contingency) * yml["costs"]["acr_cost_peravgmonth_usd"]:.2f}
+
+Total cost with {contingency}% contingency:
+${(1.0 + contingency) * (yml["costs"]["cluster_cost_permonth_usd"] + yml["costs"]["acr_cost_peravgmonth_usd"]):.2f}
+""")
 
 
 if __name__ == "__main__":
@@ -105,11 +107,11 @@ if __name__ == "__main__":
             print(exc)
 
     # Calculate costs
-    billing_info = calculate_costs(billing_info, contingency)
+    billing_info = calculate_costs(billing_info)
 
     # Print Costs
-    summary_stats(billing_info, args.contingency)
+    summary_stats(billing_info, args.contingency, args.months)
 
-    # Output calculations
-    with open("hub23_billing_calc.yaml", "w") as stream:
-        yaml.dump(billing_info, stream)
+    # # Output calculations
+    # with open("hub23_billing_calc.yaml", "w") as stream:
+    #     yaml.dump(billing_info, stream)
