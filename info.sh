47f5142707f140fe539e2323f2d8f7dcc6c41bc4
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-03 17:08:44 +0100

    info.sh no longer requires hub23 as an arg

--- info.sh
+++ info.sh
@@ -2,9 +2,9 @@
 
 # Print pods
 echo "--> Printing pods"
-kubectl get pods -n ${1}
+kubectl get pods -n hub23
 echo
 
 # Get IP addresses of both the JupyterHub and BinderHub
-echo "JupyterHub IP: " `kubectl --namespace=${1} get svc proxy-public | awk '{ print $4}' | tail -n 1`
-echo "Binder IP: " `kubectl --namespace=${1} get svc binder | awk '{ print $4}' | tail -n 1`
+echo "JupyterHub IP: " `kubectl --namespace=hub23 get svc proxy-public | awk '{ print $4}' | tail -n 1`
+echo "Binder IP: " `kubectl --namespace=hub23 get svc binder | awk '{ print $4}' | tail -n 1`
