db8e488be823e58195af7962d26e371ecad74e63
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-03 16:03:04 +0100

    Add info script
    info.sh takes a kubectl namespace as an argument and prints the
    k8s pods and the binder/jupyterhub IP addresses

---
+++ info.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Print pods
+echo "--> Printing pods"
+kubectl get pods -n ${1}
+echo
+
+# Get IP addresses of both the JupyterHub and BinderHub
+echo "Jupyterhub IP: " `kubectl --namespace=${1} get svc proxy-public | awk '{ print $4}' | tail -n 1`
+echo "Binderhub IP: " `kubectl --namespace=${1} get svc binder | awk '{ print $4}' | tail -n 1`
