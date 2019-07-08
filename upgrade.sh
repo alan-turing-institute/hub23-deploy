21b3d2a603b62c38faca167c71ad49ab69000ab3
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-08 16:13:49 +0100

    Add binder DNS name to upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -5,4 +5,4 @@ helm upgrade hub23 jupyterhub/binderhub --version=$1 -f .secret/secret.yaml -f .
 echo
 kubectl get pods -n hub23
 echo
-echo "Binder IP: " `kubectl get svc binder -n hub23 | awk '{ print $4}' | tail -n 1`
+echo "Binder IP (binder.hub23.turing.ac.uk): " `kubectl get svc binder -n hub23 | awk '{ print $4}' | tail -n 1`
