1a4fe0506b76848d1c68718338e58c34659c914b
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-08 16:11:46 +0100

    Add DNS names to info.sh

--- info.sh
+++ info.sh
@@ -6,5 +6,6 @@ kubectl get pods -n hub23
 echo
 
 # Get IP addresses of both the JupyterHub and BinderHub
-echo "JupyterHub IP: " `kubectl --namespace=hub23 get svc proxy-public | awk '{ print $4}' | tail -n 1`
-echo "Binder IP: " `kubectl --namespace=hub23 get svc binder | awk '{ print $4}' | tail -n 1`
+echo "--> Printing IP addresses"
+echo "JupyterHub IP (hub.hub23.turing.ac.uk): " `kubectl --namespace=hub23 get svc proxy-public | awk '{ print $4}' | tail -n 1`
+echo "Binder IP (binder.hub23.turing.ac.uk): " `kubectl --namespace=hub23 get svc binder | awk '{ print $4}' | tail -n 1`
