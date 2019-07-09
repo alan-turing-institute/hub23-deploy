80595302c7ec2159826460383841129d804a7715
Sarah Gibson <sgibson@turing.ac.uk>
2019-07-09 13:16:26 +0100

    Delete unused files

--- Hub23/files/etc/jupyter/jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-import os
-c.NotebookApp.extra_template_paths.append('/etc/jupyter/templates')
-c.NotebookApp.jinja_template_vars.update({
-    'binder_url': os.environ.get('BINDER_URL', 'http://binder.hub23.turing.ac.uk'),
-})
