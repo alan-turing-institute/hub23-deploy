9d7a6458460cd70720fe721e487c868ad0848f70
Sarah Gibson <sgibson@turing.ac.uk>
2019-08-01 14:08:30 +0100

    Add generate-configs.py

--- generate-configs.py
+++ generate-configs.py
@@ -1,162 +1,176 @@
 import os
 import json
+import argparse
 import subprocess
 
-SUBSCRIPTION = "Turing-BinderHub"
-RESOURCE_GROUP = "Hub23"
-VAULT_NAME = "hub23-keyvault"
-CLUSTER_NAME = "hub23cluster"
-REGISTRY_NAME = "hub23registry"
-IMAGE_PREFIX = "hub23/binder-dev"
-ORG_NAME = "binderhub-test-org"
-HUB_NAME = "hub23"
-JUPYTERHUB_IP = "hub.hub23.turing.ac.uk"
-BINDER_IP = "binder.hub23.turing.ac.uk"
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=""
+    )
 
-secret_names = [
-    "apiToken",
-    "secretToken",
-    "binderhub-access-token",
-    "github-client-id",
-    "github-client-secret",
-    "SP-appID",
-    "SP-key"
-]
+    parser.add_argument(
+        "-v",
+        "--vault-name",
+        type=str,
+        default="hub23-keyvault",
+        help=""
+    )
+    parser.add_argument(
+        "-r",
+        "--registry-name",
+        type=str,
+        default="hub23registry",
+        help=""
+    )
+    parser.add_argument(
+        "-p",
+        "--image-prefix",
+        type=str,
+        default="hub23/binder-dev",
+        help=""
+    )
+    parser.add_argument(
+        "-o",
+        "--org-name",
+        type=str,
+        default="binderhub-test-org",
+        help=""
+    )
+    parser.add_argument(
+        "-j",
+        "--jupyterhub-ip",
+        type=str,
+        default="hub.hub23.turing.ac.uk",
+        help=""
+    )
+    parser.add_argument(
+        "-b",
+        "--binder-ip",
+        type=str,
+        default="binder.hub23.turing.ac.uk",
+        help=""
+    )
+    parser.add_argument(
+        "--identity",
+        action="store_true",
+        help=""
+    )
 
-secrets = {}
+    return parser.parse_args()
 
-# Get secrets
-for secret in secret_names:
-    # Get secret information and convert to json
-    json_out = subprocess.check_output([
-        "az", "keyvault", "secret", "show", "-n", secret,
-        "--vault-name", VAULT_NAME
-    ]).decode()
-    secret_info = json.loads(json_out)
+def get_secrets(vault_name, identity=False):
+    """Get secrets from Azure key vault
 
-    # Save secret to dictionary
-    secrets[secret] = secret_info["value"]
+    Parameters
+    ----------
+    vault_name
+        String.
+    identity
+        Boolean.
 
-config_template = f"""config:
-  BinderHub:
-    template_path: /etc/binderhub/custom/templates
-    extra_static_path: /etc/binderhub/custom/static
-    extra_static_url_prefix: /extra_static/
-    template_variables:
-      EXTRA_STATIC_URL_PREFIX: "/extra_static/"
-    use_registry: true
-    image_prefix: {REGISTRY_NAME}/{IMAGE_PREFIX}-
-    auth_enabled: true
-    hub_url: https://{JUPYTERHUB_IP}
-  DockerRegistry:
-    token_url: "https://{REGISTRY_NAME}.azurecr.io/oauth2/token?service={REGISTRY_NAME}.azurecr.io"
+    Returns
+    -------
+    secrets
+        Dictionary.
+    """
+    secret_names = [
+        "apiToken",
+        "secretToken",
+        "binderhub-access-token",
+        "github-client-id",
+        "github-client-secret",
+        "SP-appID",
+        "SP-key"
+    ]
 
-jupyterhub:
-  cull:
-    every: 660
-    timeout: 600
-    maxAge: 21600
-  hub:
-    services:
-      binder:
-        oauth_redirect_uri: "http://{BINDER_IP}/oauth_callback"
-        oauth_client_id: "binder-oath-client-test"
-    extraConfig:
-      hub_extra: |
-        c.JupyterHub.redirect_to_server = False
-      binder: |
-        from kubespawner import Kubespawner
+    secrets = {}
 
-        class BinderSpawner(KubeSpawner):
-          def start(self):
-            if 'image' in self.user_options:
-              self.image = self.user_options['image']
-            return super().start()
-        c.JupyterHub.spawner_class = BinderSpawner
-  singleuser:
-    cmd: jupyterhub-singleuser
-    memory:
-      limit: 1G
-      guarantee: 1G
-    cpu:
-      limit: .5
-      guarantee: .5
-  prePuller:
-    continuous:
-      enabled: true
-  scheduling:
-    podPriority:
-      enabled: true
-    userPlaceholder:
-      replicas: 3
-    userScheduler:
-      enabled: true
-  auth:
-    type: github
-    admin:
-      access: true
-      users:
-        - sgibson91
-    github:
-      clientId: "{secrets['github-client-id']}"
-      clientSecret: "{secrets['github-client-secret']}"
-      callbackUrl: "https://{JUPYTERHUB_IP}/hub/oauth_callback"
-      orgWhitelist:
-        - "{ORG_NAME}"
-    scopes:
-      - "read:user"
-""" + """
-initContainers:
-  - name: git-clone-templates
-    image: alpine/git
-    args:
-      - clone
-      - --single-branch
-      - --branch=master
-      - --depth=1
-      - --
-      - https://github.com/alan-turing-institute/hub23-deploy
-      - /etc/binderhub/custom
-    securityContext:
-      runAsUser: 0
-    volumeMounts:
-      - name: custom-templates
-        mountPath: /etc/binderhub/custom
-extraVolumes:
-  - name: custom-templates
-    emptyDir: {}
-extraVolumeMounts:
-  - name: custom-templates
-    mountPath: /etc/binderhub/custom
-"""
+    # Login to Azure
+    print("Logging into Azure")
+    if identity:
+        proc = subprocess.Popen(
+            ["az", "login", "--identity"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    else:
+        proc = subprocess.Popen(
+            ["az", "login"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
 
-secret_template = f"""jupyterhub:
-  hub:
-    services:
-      binder:
-        apiToken: "{secrets['apiToken']}"
-  proxy:
-    secretToken: "{secrets['secretToken']}"
-registry:
-  url: https://{REGISTRY_NAME}.azurecr.io
-  username: {secrets['SP-appID']}
-  password: {secrets['SP-key']}
-config:
-  GitHubRepoProvider:
-    access_token: {secrets['binderhub-access-token']}
-"""
+    res = proc.communicate()
+    if proc.returncode == 0:
+        print("Successfully logged into Azure")
+    else:
+        err_msg = res[1].decode(encoding="utf-8")
+        raise Exception(err_msg)
 
-# Make a secrets folder
-if not os.path.exists(".secret"):
-    os.mkdir(".secret")
+    # Get secrets
+    for secret in secret_names:
+        print(f"Pulling secret: {secret}")
+        # Get secret information and convert to json
+        json_out = subprocess.check_output([
+            "az", "keyvault", "secret", "show", "-n", secret,
+            "--vault-name", vault_name
+        ]).decode(encoding="utf-8")
+        secret_info = json.loads(json_out)
 
-# Write the config files
-with open(os.path.join(".secret", "config.yaml"), "w") as f:
-    f.write(config_template)
+        # Save secret to dictionary
+        secrets[secret] = secret_info["value"]
 
-with open(os.path.join(".secret", "secret.yaml"), "w") as f:
-    f.write(secret_template)
+    return secrets
 
-print("Your BinderHub files have been configured:")
-for bhub_file in os.listdir(".secret"):
-    print(bhub_file)
+def main():
+    args = parse_args()
+
+    # Make a secrets folder
+    secret_dir = ".secret"
+    if not os.path.exists(secret_dir):
+        print(f"Creating directory: {secret_dir}")
+        os.mkdir(secret_dir)
+        print(f"Created directory: {secret_dir}")
+    else:
+        print(f"Directory already exists: {secret_dir}")
+
+    secrets = get_secrets(args.vault_name, args.identity)
+
+    print("Generating configuration files")
+    for filename in ["config", "secret"]:
+        print(f"Reading template file for: {filename}")
+
+        with open(f"{filename}-template.yaml", "r") as f:
+            template = f.read()
+
+        if filename == "config":
+            template = template.format(
+                registry_name=args.registry_name,
+                image_prefix=args.image_prefix,
+                jupyterhub_ip=args.jupyterhub_ip,
+                binder_ip=args.binder_ip,
+                github_client_id=secrets["github-client-id"],
+                github_client_secret=secrets["github-client-secret"],
+                org_name=args.org_name
+            )
+
+        elif filename == "secret":
+            template = template.format(
+                apiToken=secrets["apiToken"],
+                secretToken=secrets["secretToken"],
+                registry_name=args.registry_name,
+                username=secrets["SP-appID"],
+                password=secrets["SP-key"],
+                accessToken=secrets["binderhub-access-token"]
+            )
+        else:
+            raise FileNotFoundError("Unknown file")
+
+        print(f"Writing YAML file for: {filename}")
+        with open(os.path.join(secret_dir, f"{filename}.yaml"), "w") as f:
+            f.write(template)
+
+    print(f"BinderHub files have been configured: {os.listdir(secret_dir)}")
+
+if __name__ == "__main__":
+    main()
