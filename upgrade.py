92e849307f642a2ae31c10e846a9d95b1ad2f5bc
Sarah Gibson <sgibson@turing.ac.uk>
2019-08-21 09:37:26 +0100

    Refactor upgrade.py into class

--- upgrade.py
+++ upgrade.py
@@ -58,145 +58,145 @@ def parse_args():
 
     return parser.parse_args()
 
-def find_version():
-    """Find the latest Helm Chart version deployed
+class Upgrade(object):
+    def __init__(self, argsDict):
+        self.hub_name = argsDict["hub_name"]
+        self.cluster_name = argsDict["cluster_name"]
+        self.resource_group = argsDict["resource_group"]
+        self.identity = argsDict["identity"]
+        self.dry_run = argsDict["dry_run"]
 
-    Returns
-    -------
-    version: string
-    """
-    from yaml import safe_load as load
+        if argsDict["version"] is None:
+            self.find_version()
+        else:
+            self.version = argsDict["version"]
 
-    with open("changelog.txt", "r") as f:
-        changelog = load(f)
+    def find_version(self):
+        from yaml import safe_load as load
 
-    keys = list(changelog.keys())
-    return changelog[keys[-1]]
+        with open("changelog.txt", "r") as f:
+            changelog = load(f)
 
-def azure_setup(cluster_name, resource_group, identity=False):
-    """Setup kubectl and helm to access BinderHub cluster
+        keys = list(changelog.keys())
+        self.version = changelog[keys[-1]]
 
-    Parameters
-    ----------
-    cluster_name: string
-    resource_group: string
-    identity: boolean
-    """
-    login_cmd = ["az", "login"]
+    def upgrade(self):
+        if self.dry_run:
+            logging.info("THIS IS A DRY-RUN. HELM CHART WILL NOT BE UPGRADED.")
 
-    if identity:
-        login_cmd.append("--identity")
-        logging.info("Logging into Azure with a Managed System Identity")
-    else:
-        logging.info("Logging into Azure")
+        # Setup Azure and Helm Chart
+        self.azure_setup()
+        self.pull_helm_chart()
 
-    result = run_cmd(login_cmd)
-    if result["returncode"] == 0:
-        logging.info("Successfully logged into Azure")
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+        # Helm Upgrade Command
+        helm_upgrade_cmd = [
+            "helm", "upgrade", self.hub_name, "jupyterhub/binderhub",
+            f"--version={self.version}",
+            "-f", os.path.join(".secret", "secret.yaml"),
+            "-f", os.path.join(".secret", "config.yaml"),
+            "--wait"
+        ]
 
-    logging.info(f"Setting kubectl context for: {cluster_name}")
-    cmd = [
-        "az", "aks", "get-credentials", "-n", cluster_name, "-g", resource_group
-    ]
-    result = run_cmd(cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+        if self.dry_run:
+            helm_upgrade_cmd.append("--dry-run")
+            logging.info("Performing a dry-run helm upgrade")
+        else:
+            logging.info("Upgrading helm chart")
 
-    logging.info("Initialising Helm")
-    cmd = ["helm", "init", "--client-only"]
-    result = run_cmd(cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+        result = run_cmd(helm_upgrade_cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-def main():
-    args = parse_args()
+        self.print_pods()
+        self.find_ip_addresses()
 
-    if args.dry_run:
-        logging.info("THIS IS A DRY-RUN. HELM CHART WILL NOT BE UPGRADED.")
+    def azure_setup(self):
+        login_cmd = ["az", "login"]
 
-    # Setup Azure
-    azure_setup(args.cluster_name, args.resource_group, identity=args.identity)
+        if self.identity:
+            login_cmd.append("--identity")
+            logging.info("Logging into Azure with a Managed System Identity")
+        else:
+            logging.info("Logging into Azure")
 
-    if args.version is None:
-        # Find most recent deployed version
-        version = find_version()
+        result = run_cmd(login_cmd)
+        if result["returncode"] == 0:
+            logging.info("Successfully logged into Azure")
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-    # Pulling/updating Helm Chart repo
-    logging.info("Adding and updating JupyterHub/BinderHub Helm Chart")
-    cmd = [
-        "helm", "repo", "add", "jupyterhub", "https://jupyterhub.github.io/helm-chart"
-    ]
-    result = run_cmd(cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
+        logging.info(f"Setting kubectl context for: {self.cluster_name}")
+        cmd = [
+            "az", "aks", "get-credentials", "-n", self.cluster_name, "-g",
+            self.resource_group
+        ]
+        result = run_cmd(cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-    cmd = ["helm", "repo", "update"]
-    result = run_cmd(cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+        logging.info("Initialising Helm")
+        cmd = ["helm", "init", "--client-only"]
+        result = run_cmd(cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-    # Helm Upgrade Command
-    helm_upgrade_cmd = [
-        "helm", "upgrade", args.hub_name, "jupyterhub/binderhub",
-        "-f", os.path.join(".secret", "secret.yaml"),
-        "-f", os.path.join(".secret", "config.yaml"),
-        "--wait"
-    ]
+    def pull_helm_chart(self):
+        # Pulling/updating Helm Chart repo
+        logging.info("Adding and updating JupyterHub/BinderHub Helm Chart")
+        cmd = [
+            "helm", "repo", "add", "jupyterhub",
+            "https://jupyterhub.github.io/helm-chart"
+        ]
+        result = run_cmd(cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
 
-    if args.version is None:
-        helm_upgrade_cmd.append(f"--version={version}")
-    else:
-        helm_upgrade_cmd.append(f"--version={args.version}")
+        cmd = ["helm", "repo", "update"]
+        result = run_cmd(cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-    if args.dry_run:
-        helm_upgrade_cmd.append("--dry-run")
-        logging.info("Performing a dry-run helm upgrade")
-    else:
-        logging.info("Upgrading helm chart")
+    def print_pods(self):
+        # Print the pods
+        logging.info("Fetching the Kubernetes pods")
+        cmd = ["kubectl", "get", "pods", "-n", self.hub_name]
+        result = run_cmd(cmd)
+        if result["returncode"] == 0:
+            logging.info(result["output"])
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
-    result = run_cmd(helm_upgrade_cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+    def find_ip_addresses(self):
+        # Fetching the Binder IP address
+        logging.info("Fetching the Binder IP address")
+        kubectl_cmd = ["kubectl", "get", "svc", "binder", "-n", self.hub_name]
+        awk_cmd = ["awk", "{ print $4}"]
+        tail_cmd = ["tail", "-n", "1"]
 
-    # Print the pods
-    logging.info("Fetching the Kubernetes pods")
-    cmd = ["kubectl", "get", "pods", "-n", args.hub_name]
-    result = run_cmd(cmd)
-    if result["returncode"] == 0:
-        logging.info(result["output"])
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
-
-    # Fetching the Binder IP address
-    logging.info("Fetching the Binder IP address")
-    kubectl_cmd = ["kubectl", "get", "svc", "binder", "-n", args.hub_name]
-    awk_cmd = ["awk", "{ print $4}"]
-    tail_cmd = ["tail", "-n", "1"]
-
-    result = run_pipe_cmd([kubectl_cmd, awk_cmd, tail_cmd])
-    if result["returncode"] == 0:
-        logging.info(f"Binder IP: {result['output']}")
-    else:
-        logging.error(result["err_msg"])
-        raise Exception(result["err_msg"])
+        result = run_pipe_cmd([kubectl_cmd, awk_cmd, tail_cmd])
+        if result["returncode"] == 0:
+            logging.info(f"Binder IP: {result['output']}")
+        else:
+            logging.error(result["err_msg"])
+            raise Exception(result["err_msg"])
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    bot = Upgrade(vars(args))
+    bot.upgrade()
