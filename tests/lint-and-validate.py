44f59feecdbb7c08bd4f75dde78a1d62b8d951fb
Sarah Gibson <sgibson@turing.ac.uk>
2019-11-25 20:33:59 +0000

    Complete lint and validate script

--- tests/lint-and-validate.py
+++ tests/lint-and-validate.py
@@ -53,7 +53,6 @@ def parse_args():
         help="Specify helm values in a YAML file (can specify multiple)",
     )
     parser.add_argument(
-        "-k",
         "--kubernetes-version",
         default="1.14.6",
         help="Version of Kubernetes to validate against",
@@ -70,3 +69,67 @@ def parse_args():
     )
 
     return parser.parse_args()
+
+
+def lint(yamllint_config, values, kubernetes_version, output_dir, debug):
+    """Calls `helm lint`, `helm template`, `yamllint` and `kubeval`."""
+    print("### Clearing output directory")
+    check_call(["mkdir", "-p", output_dir])
+    check_call(["rm", "-rf", os.path.join(output_dir, "*")])
+
+    print("### Linting started")
+    print("### 1/4 - helm lint")
+    helm_lint_cmd = [
+        "helm",
+        "lint",
+        os.path.join(os.pardir, "hub23-chart"),
+        "--values",
+        values,
+    ]
+    if debug:
+        helm_lint_cmd.append("--debug")
+    check_call(helm_lint_cmd)
+
+    print("### 2/4 - helm template")
+    helm_template_cmd = [
+        "helm",
+        "template",
+        os.path.join(os.pardir, "hub23-chart"),
+        "--value",
+        values,
+        "--output-dir",
+        output_dir,
+    ]
+    if debug:
+        helm_template_cmd.append("--debug")
+    check_call(helm_template_cmd)
+
+    print("### 3/4 - yamllint")
+    check_call(["yamllint", "-c", yamllint_config, output_dir])
+
+    print("### 4/4 kubeval")
+    for filename in glob.iglob(
+        os.path.join(output_dir, "**", "*.yaml"), recursive=True
+    ):
+        check_call(
+            [
+                "kubeval",
+                filename,
+                "--kubernetes-version",
+                kubernetes_version,
+                "--strict",
+            ]
+        )
+
+    print("\n### Linting and validation of templates finished: All good!")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    lint(
+        args.yamllint_config,
+        args.values,
+        args.kubernetes_version,
+        args.output_dir,
+        args.debug,
+    )
