ed2e0d56718df9e7920f7da37f40397773ddcc8f
Sarah Gibson <sgibson@turing.ac.uk>
2020-02-13 19:11:26 +0000

    Add exceptions for flags used with the incorrect subcommand

--- scripts/hub_manager/cli.py
+++ scripts/hub_manager/cli.py
@@ -18,12 +18,12 @@ parser.add_argument(
     help="The management action to run on the BinderHub",
 )
 parser.add_argument(
+    "-v", "--verbose", action="store_true", help="Turn on logging output"
+)
+parser.add_argument(
     "--identity",
     action="store_true",
     help="Login to Azure with a Managed System Identity",
-)
-parser.add_argument(
-    "--verbose", "-v", action="store_true", help="Turn on logging output"
 )
 parser.add_argument(
     "--dry-run",
@@ -41,6 +41,20 @@ def main():
     """Main function"""
     args = parser.parse_args(sys.argv[1:])
     obj = HubManager(vars(args))
+
+    if args.action != "helm-upgrade":
+        if args.dry_run:
+            raise Exception(
+                "--dry-run flag can only be used with helm-upgrade action"
+            )
+        elif args.debug:
+            raise Exception(
+                "--debug flag can only be used with helm-upgrade action"
+            )
+        elif args.dry_run and args.debug:
+            raise Exception(
+                "--dry-run and --debug flags can only be used with helm-upgrade action"
+            )
 
     if args.action == "generate-config-files":
         obj.generate_config_files()
