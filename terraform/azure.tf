02db5c72fd71b63e7d9a313fab2223f2f5223564
Sarah Gibson <sgibson@turing.ac.uk>
2020-10-27 20:36:56 +0000

    Add terraform providers

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1,3 +1,18 @@
+terraform {
+    required_version = ">= 0.13"
+
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "2.25.0"
+        }
+        # kubernetes = {
+        #     source  = "hashicorp/kubernetes"
+        #     version = "1.13.2"
+        # }
+    }
+}
+
 provider "azurerm" {
     version = "~> 2.0"
     features {}
